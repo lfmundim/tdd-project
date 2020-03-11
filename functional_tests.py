5568a000b3b5f5cb8143828bbcba775f9a931ed8
Lucas Mundim <lucasfmundim@ufmg.br>
2020-03-11 19:35:19 -0300

    add geckodriver path to enable test

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 
-browser = webdriver.Firefox()
+browser = webdriver.Firefox(executable_path = './geckodriver')
 browser.get('http://localhost:8000')
 
 assert 'Django' in browser.title
