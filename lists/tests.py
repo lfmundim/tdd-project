f4d6228cae37eecb18d8eb79bd8eb513d7022a8b
Lucas Mundim <lucasfmundim@ufmg.br>
2020-09-13 17:47:16 -0300

    Implementando o Novo Projeto: Ajustando o Resto do Sistema

--- lists/tests.py
+++ lists/tests.py
@@ -1,24 +1,11 @@
 from django.test import TestCase
-from lists.models import Item
+from lists.models import Item, List
 
 class HomePageTest(TestCase):
 
     def test_uses_home_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
-
-    def test_can_save_a_POST_request(self):
-        self.client.post('/', data={'item_text': 'A new list item'})
-
-        self.assertEqual(Item.objects.count(), 1)
-        new_item = Item.objects.first()
-        self.assertEqual(new_item.text, 'A new list item')
-
-    def test_redirects_after_POST(self):
-        response = self.client.post('/', data={'item_text': 'A new list item'})
-
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
 
 class ListAndItemModelsTest(TestCase):
 
@@ -52,8 +39,9 @@ class ListAndItemModelsTest(TestCase):
 class ListViewTest(TestCase):
 
     def test_displays_all_items(self):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
+        my_list = List.objects.create()
+        Item.objects.create(text='itemey 1', list=my_list)
+        Item.objects.create(text='itemey 2', list=my_list)
 
         response = self.client.get('/lists/the-only-list-in-the-world/')
 
